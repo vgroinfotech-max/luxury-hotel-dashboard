d870063ee7165022a37ae5af5047e160143ed89d
jeevikaJ8 <jeevikag819@gmail.com>
2026-03-10 17:29:57 +0530

    update

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="jeevi_28",
+        password="pass",
         database="hotel_booking_system",
         port=3306
     )
